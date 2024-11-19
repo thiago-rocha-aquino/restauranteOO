2e7e989a4cc2514973ac8fa2b126208bd323a908
Thiago Rocha de Aquino <91508287+thiago-rocha-aquino@users.noreply.github.com>
2024-11-19 17:14:23 -0300

    Update restaurante.py

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,13 +1,35 @@
-class restaurante:
-  nome = ""
-  categoria = ""
-  ativo = False
 
-restaurante_praca = restaurante()
-restaurante_praca.nome = "Praça"
-restaurante_praca.categoria = "Gourmet"
+class Restaurante:
+  restaurantes = []
 
-restaurante_vila = restaurante()
+  def __init__(self, nome, categoria,):
+    self._nome = nome.title()
+    self.categoria = categoria.upper()
+    self._ativo = False
+    Restaurante.restaurantes.append(self)
 
-restaurantes = [restaurante_praca, restaurante_vila]
+  def __str__(self):
+    return f'{self._nome} | {self.categoria}'
+  
+  @classmethod
+  def listar_restaurantes(cls):
+    print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}')
+    for restaurante in cls.restaurantes:
+        print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+
+  @property
+  def ativo(self):
+    return "☑" if self._ativo  else "☐"
+  
+  def alternar_estado(self):
+    self._ativo = not self._ativo
+
+          
+restaurante_praca = Restaurante("Praça", "Gourmet")
+restaurante_praca.alternar_estado()
+restaurante_vila = Restaurante("Pizza Express", "Italiana")
+
+
+Restaurante.listar_restaurantes()
+
 
