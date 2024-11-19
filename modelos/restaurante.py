1b26cc79e7bc8e4ff4cc8999adab1828038f4a16
Thiago Rocha de Aquino <91508287+thiago-rocha-aquino@users.noreply.github.com>
2024-11-19 13:22:11 -0300

    Add files via upload

---
+++ modelos/restaurante.py
@@ -0,0 +1,13 @@
+class restaurante:
+  nome = ""
+  categoria = ""
+  ativo = False
+
+restaurante_praca = restaurante()
+restaurante_praca.nome = "Praça"
+restaurante_praca.categoria = "Gourmet"
+
+restaurante_vila = restaurante()
+
+restaurantes = [restaurante_praca, restaurante_vila]
+
