0f2c9f2d554b06b73cdff6764e79b5bb4b8ea510
Thiago Rocha de Aquino <91508287+thiago-rocha-aquino@users.noreply.github.com>
2024-12-19 12:08:29 -0300

    Add files via upload

---
+++ modelos/restaurante.py
@@ -0,0 +1,49 @@
+from modelos.avaliacao import Avaliacao
+
+class Restaurante:
+  restaurantes = []
+
+  def __init__(self, nome, categoria,):
+    self._nome = nome.title()
+    self.categoria = categoria.upper()
+    self._ativo = False
+    self._avaliacao = []
+    Restaurante.restaurantes.append(self)
+
+  def __str__(self):
+    return f'{self._nome} | {self.categoria}'
+  
+  @classmethod
+  def listar_restaurantes(cls):
+    print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}')
+    for restaurante in cls.restaurantes:
+        print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+
+  @property
+  def ativo(self):
+    return "☑" if self._ativo  else "☐"
+  
+  def alternar_estado(self):
+    self._ativo = not self._ativo
+
+  def receber_avaliacao(self, cliente, nota):
+    avaliacao = Avaliacao(cliente, nota)
+    self._avaliacao.append(avaliacao)
+    
+  @property
+  def media_avaliacoes(self):
+    if not self._avaliacao:
+      return 0
+    soma_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+    quantidade_notas = len(self._avaliacao)
+    media = round(soma_notas / quantidade_notas, 1)
+    return media
+
+
+          
+restaurante_praca = Restaurante("Praça", "Gourmet")
+restaurante_praca.alternar_estado()
+restaurante_vila = Restaurante("Pizza Express", "Italiana")
+
+
+Restaurante.listar_restaurantes()
